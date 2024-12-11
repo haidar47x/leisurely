37e49b2bf0eff67341e8542ea798116fef3b3fa9
Haidar Ali <haidar47x@gmail.com>
2024-12-11 16:10:22 +0500

    Init

---
+++ src/test/java/net/haidarali/myriad/MyriadApplicationTests.java
@@ -0,0 +1,13 @@
+package net.haidarali.myriad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyriadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
