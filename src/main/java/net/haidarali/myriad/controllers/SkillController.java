e2bf029befc0ad42a03c56843c7be7922bcedafe
Haidar Ali <haidar47x@gmail.com>
2024-12-16 23:26:25 +0500

    Added placeholder SkillController

---
+++ src/main/java/net/haidarali/myriad/controllers/SkillController.java
@@ -0,0 +1,22 @@
+package net.haidarali.myriad.controllers;
+
+import net.haidarali.myriad.models.Skill;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+@RestController
+public class SkillController {
+
+    private final Logger logger = Logger.getLogger(this.getClass().getName());
+
+    @PostMapping("/skill")
+    public ResponseEntity<Skill> createSkill(@RequestBody Skill skill) {
+        return ResponseEntity.ok().body(skill);
+    }
+
+}
--- src/main/java/net/haidarali/myriad/controllers/UserController.java
+++ src/main/java/net/haidarali/myriad/controllers/UserController.java
@@ -7,16 +7,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.logging.Logger;
+
 @RestController
 public class UserController {
 
+    private final Logger logger = Logger.getLogger(this.getClass().getName());
+
     @GetMapping("/user")
     public ResponseEntity<User> user(@RequestBody User user) {
+        logger.info(user.toString());
         return ResponseEntity.accepted().body(user);
     }
 
     @PostMapping("/update-user")
     public ResponseEntity<User> updateUser(@RequestBody User user) {
+        logger.info(user.toString());
         user.setUsername(user.getUsername());
         return ResponseEntity.accepted().body(user);
     }
